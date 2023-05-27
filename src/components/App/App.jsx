4e641e9d14b8eafdc1cf6354da7ec0181e3f3267
Олександр <moskaletzsanych@gmail.com>
2023-05-27 19:17:24 +0300

    SharedLayout-is-done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,27 +2,19 @@ import Home from 'components/pages/Home';
 import MovieDetails from 'components/pages/MovieDetails';
 import Movies from 'components/pages/Movies';
 import NotFound from 'components/pages/NotFound';
-import { Route, Routes, NavLink } from 'react-router-dom';
-import { Container } from './App.styled';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 
 const App = () => {
   return (
-    <Container>
-      <nav>
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-        <NavLink to="/movies" end>
-          Movies
-        </NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Container>
+      </Route>
+    </Routes>
   );
 };
 
