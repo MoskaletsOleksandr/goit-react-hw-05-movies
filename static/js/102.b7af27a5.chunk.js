be656f89953595967aa2b60737de02b9da7d28f7
MoskaletsOleksandr <116104431+MoskaletsOleksandr@users.noreply.github.com>
2023-05-28 18:14:20 +0000

    Deploying to gh-pages from @ MoskaletsOleksandr/goit-react-hw-05-movies@8a0145f0b8deba67d3e672c5f0bbe9410a2d1be4 🚀

---
+++ static/js/102.b7af27a5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[102],{102:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var r,o=n(87),c=n(168),i=n(686).Z.h1(r||(r=(0,c.Z)(["\n  text-align: center;\n\n  margin-bottom: 20px;\n"]))),u=n(184),a=function(t){var e=t.title;return(0,u.jsx)(i,{children:e})},l=function(){return(0,u.jsxs)("div",{children:[(0,u.jsx)(o.rU,{to:"/",children:"Go to Home"}),(0,u.jsx)(a,{title:"NotFound"})]})}}}]);
+//# sourceMappingURL=102.b7af27a5.chunk.js.map
