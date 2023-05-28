d2ecde0a56a6e61af770efea39e3e418a3b1a319
Олександр <moskaletzsanych@gmail.com>
2023-05-28 19:59:31 +0300

    fix

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { searchMovieReviews } from "services/themoviedb-api";
 import { ReviewList, ReviewItem, Author, Content } from "./Reviews.styled";
 
